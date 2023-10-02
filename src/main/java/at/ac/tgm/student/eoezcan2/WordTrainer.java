31679e33364e9f7ec1b6e32ce2d36228175c1bd7
Emre Özcan <67864140+eoezcan2@users.noreply.github.com>
2023-10-02 10:11:31 +0200

    Add shuffle function

--- src/main/java/at/ac/tgm/student/eoezcan2/WordTrainer.java
+++ src/main/java/at/ac/tgm/student/eoezcan2/WordTrainer.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class WordTrainer {
 
     private ArrayList<WordCard> wordCards;
+    private SaveStrategy saveStrategy;
     private String filePath;
     private int currentCard;
     private int correctAnswers;
@@ -13,11 +14,34 @@ public class WordTrainer {
 
     public WordTrainer(String filePath) {
         this.filePath = filePath;
-        this.wordCards = new ArrayList<>();
+        this.wordCards = new ArrayList<WordCard>();
+        this.saveStrategy = new JsonSave();
         this.currentCard = 0;
         this.correctAnswers = 0;
         this.wrongAnswers = 0;
         this.tries = 0;
+    }
+
+    private void randomizeCards() {
+        // Shuffle array
+        for (int i = 0; i < this.wordCards.size(); i++) {
+            int randomIndexToSwap = (int) (Math.random() * this.wordCards.size());
+            WordCard temp = this.wordCards.get(randomIndexToSwap);
+            this.wordCards.set(randomIndexToSwap, this.wordCards.get(i));
+            this.wordCards.set(i, temp);
+        }
+    }
+
+    public WordCard getCardAt(int index) {
+        return this.wordCards.get(index);
+    }
+
+    public SaveStrategy getSaveStrategy() {
+        return saveStrategy;
+    }
+
+    public void setSaveStrategy(SaveStrategy saveStrategy) {
+        this.saveStrategy = saveStrategy;
     }
 
     public ArrayList<WordCard> getWordCards() {
