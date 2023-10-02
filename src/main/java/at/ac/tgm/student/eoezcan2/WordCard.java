c7bdfb9cb36bf112a971e172628d582a3b8fe481
Emre Özcan <67864140+eoezcan2@users.noreply.github.com>
2023-10-02 09:38:22 +0200

    Add WordCard model

---
+++ src/main/java/at/ac/tgm/student/eoezcan2/WordCard.java
@@ -0,0 +1,37 @@
+package at.ac.tgm.student.eoezcan2;
+
+public class WordCard {
+
+    private String url;
+    private String word;
+
+    public WordCard(String url, String word) {
+        // Check if URL is an image
+        setUrl(url);
+        setWord(word);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        if(!url.endsWith(".png") && !url.endsWith(".jpg") && !url.endsWith(".jpeg")) {
+            throw new IllegalArgumentException("URL is not an image!");
+        }
+        this.word = word;
+    }
+
+    public String toString() {
+        return this.word + ":" + this.url;
+    }
+
+}
